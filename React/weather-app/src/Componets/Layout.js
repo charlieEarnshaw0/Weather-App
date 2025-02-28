b5542bba257db88657505ebe0a053f55ebae20a1
charlieearnshaw0 <charlieearnshaw0@gmail.com>
2025-02-28 17:16:40 +1100

    feat the return to home element in layout will only render in non-homePage components

--- React/weather-app/src/Componets/Layout.js
+++ React/weather-app/src/Componets/Layout.js
@@ -1,13 +1,17 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 
 const Layout = () => {
+  const location = useLocation(); //React state-aware hook that should re-render when the route changes
+
   return (
     <div>
       <Outlet />
-      <h2>
-        <Link to="/">Return to Home</Link>
-      </h2>
+      {location.pathname !== "/" && (
+        <h2>
+          <Link to="/">Return to Home</Link>
+        </h2>
+      )}
     </div>
   );
 };
