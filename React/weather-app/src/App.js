36ccdcd1c5453e412f9740f671ec2c059b69f3b2
charlieearnshaw0 <charlieearnshaw0@gmail.com>
2025-02-28 14:56:09 +1100

    add users can now select their city using a input form

--- React/weather-app/src/App.js
+++ React/weather-app/src/App.js
@@ -1,10 +1,32 @@
 import React, { useState, useEffect } from "react";
 import api from "./api";
-const App = ({ city = "Wellington" }) => {
-  //Debug
-  const [weatherApiSaved, setWeatherApiSaved] = useState(true); //saves weather data in local host rather than calling the api again
+
+const App = () => {
+  const [weatherApiSaved, setWeatherApiSaved] = useState(true); //saves weather data in local host rather than calling the api again. For optimisation purposes.
 
   const [weather, setWeather] = useState(null);
+  const [city, setCity] = useState("");
+
+  const CityForm = () => {
+    const [cityInput, setCityInput] = useState(city); //the value of the input field. NOT the current city
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      console.log("Country submitted:", cityInput);
+      setCity(cityInput); //Only set city on submission
+    };
+
+    return (
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={cityInput}
+          onChange={(event) => setCityInput(event.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    );
+  };
 
   useEffect(() => {
     const fetchWeatherFromApi = async () => {
@@ -24,7 +46,8 @@ const App = ({ city = "Wellington" }) => {
       console.log(parsedWeather);
       if (
         !parsedWeather.error &&
-        parsedWeather.location.name.toLowerCase() === city.toLowerCase()
+        (parsedWeather.location.name.toLowerCase() === city.toLowerCase() ||
+          city === "")
       ) {
         setWeather(parsedWeather);
         console.log("Fetching weather data from local host...");
@@ -34,7 +57,7 @@ const App = ({ city = "Wellington" }) => {
     } else {
       fetchWeatherFromApi();
     }
-  }, []);
+  }, [city]);
 
   return (
     <div>
@@ -64,10 +87,12 @@ const App = ({ city = "Wellington" }) => {
             Visibility: {weather.current.vis_km} km ({weather.current.vis_miles}{" "}
             miles)
           </p>
+          <p>Time: {weather.current.last_updated} </p>
         </div>
       ) : (
         <p>Loading...</p>
       )}
+      <CityForm />
     </div>
   );
 };
