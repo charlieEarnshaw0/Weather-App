d566d4e30470ef0d05733c981bffddd086ef8fa6
charlieearnshaw0 <charlieearnshaw0@gmail.com>
2025-02-28 12:34:46 +1100

    add credentials and backend logic to allow react app to query weather API

---
+++ FastAPI/weatherAPI.py
@@ -0,0 +1,19 @@
+#Accessing the weather API
+
+import http.client
+
+try:
+    from credentials import rapid_api_key
+except ImportError:
+    raise ImportError("Failed to import rapid_api_key from credentials. Please ensure the credentials.py file exists and contains the rapid_api_key variable.")
+
+def getCurrentWeather(city):
+    conn = http.client.HTTPSConnection("weatherapi-com.p.rapidapi.com")
+    headers = {
+        'x-rapidapi-key': rapid_api_key,
+        'x-rapidapi-host': "weatherapi-com.p.rapidapi.com"
+    }
+    conn.request("GET", f"/current.json?q={city}", headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+    return data.decode("utf-8")
