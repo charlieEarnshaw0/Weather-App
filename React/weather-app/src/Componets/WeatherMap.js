747e04410e9046cb9df19f01af6e784f2bbbe240
charlieearnshaw0 <charlieearnshaw0@gmail.com>
2025-03-04 14:17:08 +1100

    feat loaded a simple map in weather map

--- React/weather-app/src/Componets/WeatherMap.js
+++ React/weather-app/src/Componets/WeatherMap.js
@@ -1,7 +1,22 @@
+import React, { useState } from "react";
+import { MapContainer, TileLayer, useMapEvents } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
 const WeatherMap = () => {
   return (
-    <div className="weather-map">
-      <h2>Weather Map</h2>
+    <div>
+      <h1>Weather Map</h1>
+      <MapContainer
+        center={[48, 2]}
+        zoom={13}
+        style={{ height: "100vh", width: "100vw" }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+      </MapContainer>
+      <h1>End of Map</h1>
     </div>
   );
 };
